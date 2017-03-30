5ef0c4879a5ff4c8410cc1202217cc26b7900829
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-03-30 10:05:32 +0200

    fixes

--- project90/js/app2.js
+++ project90/js/app2.js
@@ -124,8 +124,8 @@ $(function() {
                     collectedAnswers.push(dataText);
                     console.log(collectedAnswers, collectedAnswers.length);
                     createQuestions();
-                    $(this).parent().hide();
-                    $(this).parent().prev().hide();
+                    $(this).parent().remove();
+                    $(this).parent().prev().remove();
                 }
 
             } else {
