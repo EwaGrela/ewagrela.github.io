527ab675239b8f9a6cd60ecdaca52f9e1b0b9761
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-03-30 11:35:43 +0200

    looking for a bug

--- project90/js/app2.js
+++ project90/js/app2.js
@@ -134,7 +134,7 @@ $(function() {
             }
 
         })
-
+        console.log(index);
         function showResults() {
             var resultsBoard = $("<div>", {
                 class: "resultsBoard"
