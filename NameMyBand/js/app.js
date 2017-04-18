974e472aa64dac63e7dc4cfec0f4b25615fd1ee1
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-04-18 22:35:24 +0200

    put data in firebase

--- NameMyBand/js/app.js
+++ NameMyBand/js/app.js
@@ -1,189 +1,175 @@
-$(function(){
-     
-    var bandNames = {
-        questions: [
-            {
-              title: "Are you a family band?",
-              answers: ["yes", "no"]
-            },
-            { title: "What kind of music do you guys play?",
-                answers: ["pop", "dance", "grunge"]
-            },
-            { title: "Are we talking discopolo or eurodance here?",
-                answers: ["discopolo", "eurodance"]
-            },
-            { title: "Girlband or boyband?",
-                answers: ["girlband", "boyband"]
-            },
-            { title: "Are you good guys or bad boys?",
-                answers: ["good guys", "bad boys"]
-            },
-            { title: "Are you good girls or bad b&*^$%s?",
-                answers: ["nice 'n wholesome", "wild 'n nasty"]
-            },
-        ],
-        results:{
-            family:["Guys, guys! It is really simple. Just put 'the' before your family name in plural and you're all set!"],
-            grunge:["Mudslide", "Drops of Mercury", "Samsara"],
-            discopolo: ["Xero", "Vizzer", "Vixxer", "Fixer", "Audio Tele", "Play Power"],
-            eurodance: ["Captain Obvious", "25/7", "Unlimited 3", "Mister Hollywood"],
-            boybandGood: ["In Harmony", "The Voices", "Five 4 U", "Right Direction","White Knights"],
-            boybandBad: ["Bad Boyz", "ShakeDat", "InDaMix", "The Guyz", "DragonBallz", "Night Riderzz"],
-            girlbandGood:["The Harmonies", "Sweet Harmony", "Back 2 Basic", "Angel Voices", "Foundation", "The Angelics"],
-            girlbandBad: ["2HotU", "Sweet'N'Spicy", "Chilli Babe$", "PwrGrlz", "Diabolique"]
+$(function() {
 
-        }
-    }
     var bandNamingSection = $(".bandNamingSection");
     var startingButton = $(".bandNamingSection button");
-   
-    startingButton.on("click", function(){
-        console.log("hello");
-        $(this).hide();
-        $(this).parent().hide();
-        $(this).parent().prev().hide();
-        createQuestion(0);
-    })
-    var questions = bandNames.questions;
-    var results = bandNames.results;
-    function createQuestion(index){
-        var answers = questions[index].answers;
-        var article = $("<article>");
-        var buttonDiv = $("<div>", {class: "appDiv"});
-        article.appendTo($(".bandNamingSection"));
-        buttonDiv.appendTo(article)
-        var title = $("<h3>");
-        title.prependTo(article);
-        title.text(questions[index].title);
-        console.log(answers);
-        for( var i =0; i<answers.length; i++){
-            var button = $("<button>");
-            button.appendTo(buttonDiv); 
+    console.log(firebase);
+    var bandNameRef = firebase.database().ref("/bandName");
+    bandNameRef.once("value").then(function(data) {
+        bandName = data.val();
+        console.log(bandName);
+        var bandNames = bandName[0];
+        console.log(bandNames);
+        var bandQuestions = bandNames.bandQuestions;
+        var bandResults = bandNames.bandResults;
+        console.log(bandQuestions, bandResults);
 
-            button.each(function(){
-                $(this).text(answers[i]);
-                $(this).addClass(answers[i]);
-            })
-        }      
-        
-    }
+        startingButton.on("click", function() {
+            console.log("hello");
+            $(this).hide();
+            $(this).parent().hide();
+            $(this).parent().prev().hide();
+            createQuestion(0);
+        })
+        var bandQuestions = bandNames.bandQuestions;
+        var bandResults = bandNames.bandResults;
 
+        function createQuestion(index) {
+            var answers = bandQuestions[index].answers;
+            var article = $("<article>");
+            var buttonDiv = $("<div>", {
+                class: "appDiv"
+            });
+            article.appendTo($(".bandNamingSection"));
+            buttonDiv.appendTo(article)
+            var title = $("<h3>");
+            title.prependTo(article);
+            title.text(bandQuestions[index].title);
+            console.log(answers);
+            for (var i = 0; i < answers.length; i++) {
+                var button = $("<button>");
+                button.appendTo(buttonDiv);
 
-function generateResults(){
-    var resultsArticle = $("<article>", {class:"resultsArticle"});
-    resultsArticle.appendTo(bandNamingSection);
-}
+                button.each(function() {
+                    $(this).text(answers[i]);
+                    $(this).addClass(answers[i]);
+                })
+            }
 
-bandNamingSection.on("click", ".yes", function(){
-        $(this).parent().parent().hide();
-        generateResults();
-        var randomIndex = Math.floor(Math.random() * results.family.length);
-        $(".resultsArticle").text(results.family[randomIndex]);
-        createHomeBtns($(".resultsArticle"));
-
-});
-
-bandNamingSection.on("click", ".no", function(){
-        $(this).parent().parent().hide();
-        createQuestion(1);
-        createHomeBtns($(".resultsArticle"));
-
-})
-bandNamingSection.on("click", ".grunge", function(){
-        $(this).parent().parent().hide();
-        generateResults();
-        var randomIndex = Math.floor(Math.random() * results.grunge.length);
-        $(".resultsArticle").text(results.grunge[randomIndex]);
-        createHomeBtns($(".resultsArticle"));
-
-})
-bandNamingSection.on("click", ".dance", function(){
-        
-        $(this).parent().parent().hide();
-        createQuestion(2);
-        createHomeBtns($(".resultsArticle"));
-})
-
-bandNamingSection.on("click", ".pop", function(){
-        
-        $(this).parent().parent().hide();
-        createQuestion(3);
-        createHomeBtns($(".resultsArticle"));
-
-})
-
-bandNamingSection.on("click", ".discopolo", function(){
-        
-        $(this).parent().parent().hide();
-        generateResults();
-        var randomIndex = Math.floor(Math.random() * results.discopolo.length);
-        $(".resultsArticle").text(results.discopolo[randomIndex]);
-        createHomeBtns($(".resultsArticle"));
-})
+        }
 
 
-bandNamingSection.on("click", ".eurodance", function(){
-        $(this).parent().parent().hide();
-        generateResults();
-        var randomIndex = Math.floor(Math.random() * results.eurodance.length);
-        $(".resultsArticle").text(results.eurodance[randomIndex]);
-        createHomeBtns($(".resultsArticle"));
-})
+        function generatebandResults() {
+            var bandResultsArticle = $("<article>", {
+                class: "bandResultsArticle"
+            });
+            bandResultsArticle.appendTo(bandNamingSection);
+        }
 
-bandNamingSection.on("click", ".boyband", function(){
-        $(this).parent().parent().hide();
-        createQuestion(4)
-        createHomeBtns($(".resultsArticle"));
-})
+        bandNamingSection.on("click", ".yes", function() {
+            $(this).parent().parent().hide();
+            generatebandResults();
+            var randomIndex = Math.floor(Math.random() * bandResults.family.length);
+            $(".bandResultsArticle").text(bandResults.family[randomIndex]);
+            createHomeBtns($(".bandResultsArticle"));
 
-bandNamingSection.on("click", ".girlband", function(){
-        
-        $(this).parent().parent().hide();
-        createQuestion(5)
-        createHomeBtns($(".resultsArticle"));
-})
+        });
 
-bandNamingSection.on("click", ".nice", function(){
-        
-        $(this).parent().parent().hide();
-        generateResults();
-        var randomIndex = Math.floor(Math.random() * results.girlbandGood.length);
-        $(".resultsArticle").text(results.girlbandGood[randomIndex]);
-        createHomeBtns($(".resultsArticle"));
-})
+        bandNamingSection.on("click", ".no", function() {
+            $(this).parent().parent().hide();
+            createQuestion(1);
+            createHomeBtns($(".bandResultsArticle"));
 
-bandNamingSection.on("click", ".wild", function(){
-        
-        $(this).parent().parent().hide();
-        generateResults();
-        var randomIndex = Math.floor(Math.random() * results.girlbandBad.length);
-        $(".resultsArticle").text(results.girlbandBad[randomIndex]);
-        createHomeBtns($(".resultsArticle"));
-})
+        })
+        bandNamingSection.on("click", ".grunge", function() {
+            $(this).parent().parent().hide();
+            generatebandResults();
+            var randomIndex = Math.floor(Math.random() * bandResults.grunge.length);
+            $(".bandResultsArticle").text(bandResults.grunge[randomIndex]);
+            createHomeBtns($(".bandResultsArticle"));
 
-bandNamingSection.on("click", ".good", function(){
-        $(this).parent().parent().hide();
-        generateResults();
-        var randomIndex = Math.floor(Math.random() * results.boybandGood.length);
-        $(".resultsArticle").text(results.boybandGood[randomIndex]);
-        createHomeBtns($(".resultsArticle"));
-})
+        })
+        bandNamingSection.on("click", ".dance", function() {
 
-bandNamingSection.on("click", ".bad", function(){
-        $(this).parent().parent().hide();
-        generateResults();
-        var randomIndex = Math.floor(Math.random() * results.boybandBad.length);
-        $(".resultsArticle").text(results.boybandBad[randomIndex]);
-        createHomeBtns($(".resultsArticle"));
-})
+            $(this).parent().parent().hide();
+            createQuestion(2);
+            createHomeBtns($(".bandResultsArticle"));
+        })
 
-function createHomeBtns(element) {
-        var button = $("<a>", {class:"back"});
-        button.attr("href", "https://ewagrela.github.io/NameMyBand/");
-        button.appendTo(element);
-        button.text("back")
+        bandNamingSection.on("click", ".pop", function() {
 
-}
+            $(this).parent().parent().hide();
+            createQuestion(3);
+            createHomeBtns($(".bandResultsArticle"));
+
+        })
+
+        bandNamingSection.on("click", ".discopolo", function() {
+
+            $(this).parent().parent().hide();
+            generatebandResults();
+            var randomIndex = Math.floor(Math.random() * bandResults.discopolo.length);
+            $(".bandResultsArticle").text(bandResults.discopolo[randomIndex]);
+            createHomeBtns($(".bandResultsArticle"));
+        })
+
+
+        bandNamingSection.on("click", ".eurodance", function() {
+            $(this).parent().parent().hide();
+            generatebandResults();
+            var randomIndex = Math.floor(Math.random() * bandResults.eurodance.length);
+            $(".bandResultsArticle").text(bandResults.eurodance[randomIndex]);
+            createHomeBtns($(".bandResultsArticle"));
+        })
+
+        bandNamingSection.on("click", ".boyband", function() {
+            $(this).parent().parent().hide();
+            createQuestion(4)
+            createHomeBtns($(".bandResultsArticle"));
+        })
+
+        bandNamingSection.on("click", ".girlband", function() {
+
+            $(this).parent().parent().hide();
+            createQuestion(5)
+            createHomeBtns($(".bandResultsArticle"));
+        })
+
+        bandNamingSection.on("click", ".nice", function() {
+
+            $(this).parent().parent().hide();
+            generatebandResults();
+            var randomIndex = Math.floor(Math.random() * bandResults.girlbandGood.length);
+            $(".bandResultsArticle").text(bandResults.girlbandGood[randomIndex]);
+            createHomeBtns($(".bandResultsArticle"));
+        })
+
+        bandNamingSection.on("click", ".wild", function() {
+
+            $(this).parent().parent().hide();
+            generatebandResults();
+            var randomIndex = Math.floor(Math.random() * bandResults.girlbandBad.length);
+            $(".bandResultsArticle").text(bandResults.girlbandBad[randomIndex]);
+            createHomeBtns($(".bandResultsArticle"));
+        })
+
+        bandNamingSection.on("click", ".good", function() {
+            $(this).parent().parent().hide();
+            generatebandResults();
+            var randomIndex = Math.floor(Math.random() * bandResults.boybandGood.length);
+            $(".bandResultsArticle").text(bandResults.boybandGood[randomIndex]);
+            createHomeBtns($(".bandResultsArticle"));
+        })
+
+        bandNamingSection.on("click", ".bad", function() {
+            $(this).parent().parent().hide();
+            generatebandResults();
+            var randomIndex = Math.floor(Math.random() * bandResults.boybandBad.length);
+            $(".bandResultsArticle").text(bandResults.boybandBad[randomIndex]);
+            createHomeBtns($(".bandResultsArticle"));
+        })
+
+        function createHomeBtns(element) {
+            var button = $("<a>", {
+                class: "back"
+            });
+            button.attr("href", "https://ewagrela.github.io/NameMyBand/");
+            button.appendTo(element);
+            button.text("back")
+
+        }
+    });
+
+
 
 
 });
