e4485cd8716ba1ff2fd9e0ee0a8446e824bb618c
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-04-30 12:25:29 +0200

    added info section, will start adding styles

--- PortfolioPage/js/app.js
+++ PortfolioPage/js/app.js
@@ -2,76 +2,19 @@ $(function(){
 	console.log("ok");
 
  
-var compability ={
-		questions: [
-			{title:"Tea or coffee?",
-			answers: ["tea", "coffee", "energy drinks"],
-			correct:"energy drinks"
-			},
-			{title: "You prefer to work?",
-			answers: ["orderly", "creatively", "why not both!"],
-			correct: "why not both!"
-			},
-			{ title: "Which take-away would you order?",
-			answers:["pizza", "chinese", "indian"],
-			correct: "indian"
-			},
-			{ title: "What is more important?",
-			answers: ["passion", "perfection", "50/50"],
-			correct: "50/50"
-			},
-			{ title: "Your ideal coworker is...?",
-			answers: ["extroverted", "introverted", "whatever"],
-			correct: "extroverted"
-			},
-			{ title: "You think talkative people are...",
-			answers: ["silly", "honest", "as long as they do their work, cool"],
-			correct: "as long as they do their work, cool"
-			},
-			{ title: "People graduating from bootcamps...",
-			answers: ["know the basics, hardworking", "never will be true programmers", "ignorant posers"],
-			correct: "know the basics, hardworking"
-			},
-			{ title: "You are looking for...",
-			answers: ["HTML/CSS coder", "JavaScript Dev/FrontEndDev", "someone...?"],
-			correct: "JavaScript Dev/FrontEndDev"
-			},
-			{ title: "Do you know what DRY and KISS is?",
-			answers: ["sure!", "let me google that!", "err...what?!"],
-			correct: "sure"
-			},
-			{ title: "FrontEndDevs are...",
-			answers: ["not as smart as BackEndDevs", "creative and awesome", "what is a FrontEndDev?"],
-			correct: "creative and awesome"
-			},
-
-		]
-	}
-
-	var questions = compability.questions;
-	var recordedAnswers = [];
-	var points = 0;
-	var index = 0 //index pytania;
-	var otherSections = $("section");
-	var body = $("body");
-	var grid = $(".grid");
-	var gridSection = $(".grid-section");
-	var content = $(".content");
-	var buttons = grid.find("button").not(".contentBtn");
-	var gridLinks = grid.find("a");
-	var firstButton = $("button#diamond1");
 
 	firstButton.on("click", function(){
-		content.eq(0).removeClass("invisible");
+		content.removeClass("invisible");
 		buttons.addClass("invisible");
 		gridLinks.addClass("invisible");
 
 	});
 
-	var nextBtn = $(".contentBtn");
-	nextBtn.on("click", function(){
-		$(this).parent().addClass("invisible");
-		$(this).parent().next().removeClass("invisible");
+	thirdButton.on("click", function(){
+		console.log("ok");
+		content.removeClass("invisible");
+		$(this).addClass("invisible");
+		buttons.parent().parent().addClass("invisible");
 	})
 
 	var backBtn = $(".back");
@@ -79,13 +22,14 @@ var compability ={
 		location.reload();
 	})
 
-	var quizStartingBtn = $("button#diamond3");
+	var quizStartingBtn = $("button.quizInitiator");
 	console.log(quizStartingBtn);
 	quizStartingBtn.one("click", function(){
 		console.log("ok, works");
 		$(this).addClass("invisible");
-		content.removeClass("invisible");
-		buttons.parent().parent().addClass("invisible");
+		$(this).parent().remove();;
+		//content.removeClass("invisible");
+		//buttons.parent().parent().addClass("invisible");
 		createQuestion(0);
 	})
 	
@@ -184,7 +128,7 @@ var compability ={
 		board.appendTo(content);
 		var paragraph = $("<p>", {class: "title"});
 		paragraph.appendTo(board);
-		if(points<questions.length){
+		if(points<questions.length/2){
 			var result = "It seems we are quite different, but this may mean we will learn a lot from each other and this can be very useful. It is best to check!"
 		} else if (points>=questions.length/2 && points<(questions.length/10)*8) {
 			var result ="We are not 100% compatible and this is great. We are just as similar to not disagree over petty matters and just as different to learn from our differences."
---
+++ PortfolioPage/js/variables.js
@@ -0,0 +1,62 @@
+
+var compability ={
+		questions: [
+			{title:"Tea or coffee?",
+			answers: ["tea", "coffee", "energy drinks"],
+			correct:"energy drinks"
+			},
+			{title: "You prefer to work?",
+			answers: ["orderly", "creatively", "why not both!"],
+			correct: "why not both!"
+			},
+			{ title: "Which take-away would you order?",
+			answers:["pizza", "chinese", "indian"],
+			correct: "indian"
+			},
+			{ title: "What is more important?",
+			answers: ["passion", "perfection", "50/50"],
+			correct: "50/50"
+			},
+			{ title: "Your ideal coworker is...?",
+			answers: ["extroverted", "introverted", "whatever"],
+			correct: "extroverted"
+			},
+			{ title: "You think talkative people are...",
+			answers: ["silly", "honest", "I do not mind them"],
+			correct: "honest"
+			},
+			{ title: "People graduating from bootcamps...",
+			answers: ["know the basics, hardworking", "never will be true programmers", "ignorant posers"],
+			correct: "know the basics, hardworking"
+			},
+			{ title: "You are looking for...",
+			answers: ["HTML/CSS coder", "JavaScript Dev/FrontEndDev", "someone...?"],
+			correct: "JavaScript Dev/FrontEndDev"
+			},
+			{ title: "Do you know what DRY and KISS is?",
+			answers: ["sure!", "let me google that!", "err...what?!"],
+			correct: "sure"
+			},
+			{ title: "FrontEndDevs are...",
+			answers: ["not as smart as BackEndDevs", "creative and awesome", "what is a FrontEndDev?"],
+			correct: "creative and awesome"
+			},
+
+		]
+	}
+
+	var questions = compability.questions;
+	var recordedAnswers = [];
+	var points = 0;
+	var index = 0 //index pytania;
+	var otherSections = $("section");
+	var body = $("body");
+	var grid = $(".grid");
+	var gridSection = $(".grid-section");
+	var content = $(".content");
+	console.log(content);
+	var buttons = grid.find("button").not(".contentBtn");
+	var gridLinks = grid.find("a");
+	var firstButton = $("button#diamond1");
+	var thirdButton = $("button#diamond3");
+	console.log(thirdButton);
