1d9074948f363ce2c6c5f34578ce0806b4962364
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-05-05 23:51:19 +0200

    added more elements

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM loaded");
-    var bigSquares = document.querySelectorAll(".square-big");
-    console.log(bigSquares);
-    for (var i = 0; i<bigSquares.length; i++){
-    	bigSquares[i].addEventListener("mouseover", function(){
-    			this.children[1].classList.remove("invisible");
-    	})
-    	bigSquares[i].addEventListener("mouseout", function(){
-    			this.children[1].classList.add("invisible");
-    	})
+    var grid = document.querySelector(".grid");
+    console.log(grid);
+    var links = grid.querySelectorAll("a");
+    console.log(links);
+    for( var i =0; i<links.length; i++){
+      
+       links[i].addEventListener("mouseover", function(event){
+           
+            var diamond = this.nextElementSibling;
+           
+            diamond.classList.remove("invisible");
+            if(diamond.hasAttribute("data", "hovered")){
+                diamond.classList.add("invisible");
+            }
+            
+            var timeout2 = setTimeout(function(){
+                diamond.setAttribute("data", "hovered");
+               diamond.classList.add("invisible");
+               //diamond.parentNode.removeChild(diamond);
+            }, 2200);
+
+       })
+       
     }
     
 
