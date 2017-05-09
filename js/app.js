e789d2408f22316040eb5a1e7ef52eae18dd30b6
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-05-09 22:01:17 +0200

    minor changes in welcome page and to do list

--- js/app.js
+++ js/app.js
@@ -5,25 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var links = grid.querySelectorAll("a");
    
 
-    for( var i =0; i<links.length; i++){
-      
-       links[i].addEventListener("mouseover", function(event){
-           
-            var diamond = this.nextElementSibling;
-           
-            diamond.classList.remove("invisible");
-            if(diamond.hasAttribute("data", "hovered")){
-                diamond.classList.add("invisible");
-            }
-            
-            var timeout2 = setTimeout(function(){
-                diamond.setAttribute("data", "hovered");
-               diamond.classList.add("invisible");
-            }, 2500);
-
-       })
-       
-    }
+    
     
 
 
