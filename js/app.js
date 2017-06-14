8f24541a3cec5d660ff62f4ac5e67c4f872c6eff
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-06-14 11:48:50 +0200

    added animation and hover effect

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM loaded");
-    var grid = document.querySelector(".grid");
+    const allFirstParagraphs = document.querySelectorAll("a p:nth-of-type(1)");
+    console.log(allFirstParagraphs);
+   	
+   	const allLinks = document.querySelectorAll("a");
+   	console.log(allLinks);
+   	for( var i = 0; i<allLinks.length; i++){
+   		allLinks[i].addEventListener("mouseover", function(e){
+   			console.log(this.firstElementChild, this.firstElementChild.nextElementSibling);
+   			const firstChild = this.firstElementChild;
+   			const secondChild = this.firstElementChild.nextElementSibling;
+   			firstChild.classList.add("invisible");
+   			secondChild.classList.remove("invisible");
+   		});
+
+   		allLinks[i].addEventListener("mouseleave", function(e){
+   			console.log(this.firstElementChild, this.firstElementChild.nextElementSibling);
+   			const firstChild = this.firstElementChild;
+   			const secondChild = this.firstElementChild.nextElementSibling;
+   			firstChild.classList.remove("invisible");
+   			secondChild.classList.add("invisible");
+   		});
+   	}
+
     
-    var links = grid.querySelectorAll("a");
    
-
-    
-    
-
-
 });
 
 
