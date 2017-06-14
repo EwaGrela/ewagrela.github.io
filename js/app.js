16412eef7251e979d9e8da6d43ca6556a2a0e3a6
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-06-14 21:12:48 +0200

    added script differentiating e.listeners in different devices

--- js/app.js
+++ js/app.js
@@ -1,30 +1,61 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM is loaded");
-    const allFirstParagraphs = document.querySelectorAll("a p:nth-of-type(1)");
-    console.log(allFirstParagraphs);
-   	
-   	const allLinks = document.querySelectorAll("a");
-   	console.log(allLinks);
-   	for( var i = 0; i<allLinks.length; i++){
-   		allLinks[i].addEventListener("mouseover", function(e){
-   			console.log(this.firstElementChild, this.firstElementChild.nextElementSibling);
-   			const firstChild = this.firstElementChild;
-   			const secondChild = this.firstElementChild.nextElementSibling;
-   			firstChild.classList.add("invisible");
-   			secondChild.classList.remove("invisible");
-   		});
+    const allLinks = document.querySelectorAll(".squares");
+    let clickCount = 0
 
-   		allLinks[i].addEventListener("mouseleave", function(e){
-   			console.log(this.firstElementChild, this.firstElementChild.nextElementSibling);
-   			const firstChild = this.firstElementChild;
-   			const secondChild = this.firstElementChild.nextElementSibling;
-   			firstChild.classList.remove("invisible");
-   			secondChild.classList.add("invisible");
-   		});
-   	}
+    if (matchMedia) {
+        var mq = window.matchMedia("(max-width: 595px)");
+        mq.addListener(WidthChange);
+        WidthChange(mq);
+    }
 
-    
-   
+    function WidthChange(mq) {
+        if (mq.matches) {
+            for (var i = 0; i < allLinks.length; i++) {
+                allLinks[i].addEventListener("click", makeLinksVisibleOnMobile);
+
+                allLinks[i].removeEventListener("mouseover", makeLinksVisible);
+
+                allLinks[i].removeEventListener("mouseleave", makeLinksInvisible);
+            }
+
+        } else {
+            for (var i = 0; i < allLinks.length; i++) {
+                allLinks[i].removeEventListener("click", makeLinksVisibleOnMobile);
+
+                allLinks[i].addEventListener("mouseover", makeLinksVisible);
+
+                allLinks[i].addEventListener("mouseleave", makeLinksInvisible);
+            }
+        }
+    }
+
+    function makeLinksVisibleOnMobile(event) {
+        clickCount++;
+        const firstChild = this.firstElementChild;
+        const secondChild = this.firstElementChild.nextElementSibling;
+        if (clickCount % 2 === 1) {
+            firstChild.classList.add("invisible");
+            secondChild.classList.remove("invisible");
+        } else {
+            firstChild.classList.remove("invisible");
+            secondChild.classList.add("invisible");
+        }
+
+    }
+
+
+    function makeLinksVisible() {
+        const firstChild = this.firstElementChild;
+        const secondChild = this.firstElementChild.nextElementSibling;
+        firstChild.classList.add("invisible");
+        secondChild.classList.remove("invisible");
+    }
+
+    function makeLinksInvisible() {
+        const firstChild = this.firstElementChild;
+        const secondChild = this.firstElementChild.nextElementSibling;
+        firstChild.classList.remove("invisible");
+        secondChild.classList.add("invisible");
+    }
+
 });
-
-
