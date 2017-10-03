74d1fcfc7384b6a2369bd61359a26fc34ae416bc
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-10-03 10:26:45 +0200

    added some scripts

--- LandingPage2/js/app.js
+++ LandingPage2/js/app.js
@@ -6,3 +6,44 @@ function changeDisplay(){
 	dropDownMenu.classList.toggle("invisible");
 
 };
+
+$(function(){
+	/* making the menu stick */
+	const menu = $(".menu");
+	const list = menu.find("ul.menu-list");
+	const menuPosition = menu.offset();
+	const topPosition = menuPosition.top;
+
+	$(window).on("scroll", function(event){
+		const scrollTop = $(this).scrollTop();
+		(scrollTop>topPosition) ? menu.addClass("sticky") : menu.removeClass("sticky");
+	})
+
+	$(window).on("resize", function(event){
+		const position = menu.position();
+		const scrollTop2 = $(this).scrollTop();
+		(scrollTop2>position) ? menu.addClass("sticky") : menu.removeClass("sticky");
+	})
+
+	/* making scroll to the content */
+	const links = $(".menu").find("a");
+	console.log(links);
+	links.on("click", function(event){
+		event.preventDefault();
+		const href = $(this).attr("href");
+		$("html, body").animate({
+			scrollTop: $(href).offset().top
+		}, 2500)
+	})
+
+	const scrollButton = $("#scroll-button");
+	scrollButton.on("click", function(event){
+		event.preventDefault();
+		const href = $(this).attr("href");
+		$("html, body").animate({
+			scrollTop: $(href).offset().top
+		}, 3500)
+	})
+
+})
+
